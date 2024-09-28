f4cea31b74415465304fe8430748877fb4021242
lce-j <lucie.josse@coda-student.school>
2024-09-28 21:15:28 +0200

    finir calculatrice

--- main.c
+++ main.c
@@ -2,5 +2,28 @@
 #include <stdio.h>
 
 int main() {
+   char symbol;
+   int first_number;
+   int secound_number;
+   int result;
+   printf("Bonjour, choisissez un symbole d'opération entre +, -, *, /");
+   scanf("%c", &symbol);
+   printf("Quel sont vos deux nombres ?");
+   scanf("%d", &first_number);
+   scanf("%d", &secound_number);
+
+   if (symbol == '+'){
+       result = first_number + secound_number;}
+   else if (symbol == '-'){
+       result = first_number - secound_number;}
+   else if (symbol == '*'){
+       result = first_number * secound_number;}
+   else if (symbol == '/'){
+       result = first_number / secound_number;}
+   //else if (symbol == '%'){
+       //result = first_number % secound_number;}
+   else {printf("%c n'est pas un des symboles demandés. \n", symbol);}
+
+   printf("%d %c %d = %d \n", first_number, symbol, secound_number, result);
    exit(0);
 }
